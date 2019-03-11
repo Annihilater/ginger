9d5aeaefcd9b0a6275e87b1635f9862fc73115b0
Annihilater <yanmiexingkong@gmail.com>
2019-03-11 19:08:22 +0800

    style: update book model

--- app/models/book.py
+++ app/models/book.py
@@ -11,16 +11,17 @@ from app.models.base import Base
 
 class Book(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(50), nullable=False)
-    author = Column(String(30), default='未知')
-    binding = Column(String(20))
-    publisher = Column(String(50))
-    price = Column(String(20))
-    pages = Column(Integer)
-    pubdate = Column(String(20))
+    title = Column(String(1000), nullable=False)
     isbn = Column(String(15), nullable=False, unique=True)
-    summary = Column(String(1000))
-    image = Column(String(50))
+    author = Column(String(1000), default='未名')
+    translator = Column(String(1000))
+    binding = Column(String(100))
+    publisher = Column(String(100))
+    price = Column(String(100))
+    pages = Column(String(100))
+    pubdate = Column(String(50))
+    summary = Column(String(5000))
+    image = Column(String(1000))
 
     @orm.reconstructor
     def __init__(self):
